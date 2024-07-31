8417f3453c8ab31f9ea980629c3b6a2432d51eaf
Mamush-Meshesha <mamgithub@gmail.com>
2024-07-31 11:04:08 +0300

    service and route added

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { Housinglocation } from '../housinglocation';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  template: `
+    <section class="listing">
+      <img class="listing-photo" [src]="housingLocation.photo" alt="phooto" />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+    </section>
+  `,
+  styleUrls: ['./housing-location.component.css'],
+})
+export class HousingLocationComponent {
+  @Input() housingLocation!: Housinglocation;
+}
